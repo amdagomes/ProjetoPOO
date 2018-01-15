0e9724ad88d43a5a043a29df4bb4dca426ca1a24
Amanda Gomes <amandagomes2010@gmail.com>
2018-01-15 09:08:53 -0300

    adição classe pagamento

---
+++ src/modelo/Pagamento.java
@@ -0,0 +1,43 @@
+package modelo;
+
+/**
+ *
+ * @author Amanda
+ */
+
+import java.time.LocalDate;
+
+public class Pagamento {
+    private LocalDate vencimento;
+    private float vAdicional;
+    private String mesReferente;
+
+    public Pagamento(LocalDate vencimento, float vAdicional, String mesReferente) {
+        this.vencimento = vencimento;
+        this.vAdicional = vAdicional;
+        this.mesReferente = mesReferente;
+    }
+
+    public LocalDate getVencimento() {
+        return vencimento;
+    }
+    public void setVencimento(LocalDate vencimento) {
+        this.vencimento = vencimento;
+    }
+
+    public float getvAdicional() {
+        return vAdicional;
+    }
+    public void setvAdicional(float vAdicional) {
+        this.vAdicional = vAdicional;
+    }
+
+    public String getMesReferente() {
+        return mesReferente;
+    }
+    public void setMesReferente(String mesReferente) {
+        this.mesReferente = mesReferente;
+    }
+    
+    
+}
